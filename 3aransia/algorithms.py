4dfac108511e27a0aded5430bb19b87b00a2dd01
Amine M. Boulouma <amine@boulouma.com>
2019-10-25 05:53:41 +0200

    Update on the alphabet, added moroccan to arabic translation

--- 3aransia/algorithms.py
+++ 3aransia/algorithms.py
@@ -1,6 +1,25 @@
 import pandas as pd
 import nltk
 
+from constants import *
+
+# Translate Moroccan to Arabic
+def moroccan_to_arabic(_str):
+    alphabet = pd.read_csv('data/' + MOROCCAN_ALPHABET, usecols=['MoroccanAlphabet','ArabianAlphabet'])
+    arabian_translation = []
+    for word in _str.split():
+        arabian_word = []
+        for c in word:
+            for (m, a) in zip(alphabet['MoroccanAlphabet'], alphabet['ArabianAlphabet']):
+                if c == m:
+                    arabian_word.append(a)
+        arabian_translation.append(''.join(arabian_word[::-1]))
+    
+    return ' '.join(arabian_translation[::-1])
+
+# Translate Arabic to Moroccan
+
+
 # Function to compute the distance between two words
 def word_distance(word_1, word_2):
     return nltk.edit_distance(word_1, word_2)
@@ -52,7 +71,7 @@ def validate_dictionary(dictionary):
 def run_tests():
     print(word_to_substitute('7alwa'))
     print(word_to_substitute('9o9o'))
-    print(validate_dictionary('data/dictionary.csv'))
+    print(validate_dictionary('data/' + OPEN_DICTIONARY))
     print(word_distance("7alwa", "halwa"))
     print(word_distance("Halwa", "halwa"))
     print(generate_close_words(2, ''' 3aransia 3ebass 3la 3lache 3lak 3jbek ach achno 
@@ -62,5 +81,14 @@ ghi had hada hadi hamdolillah hmd hmed hna ilyass incha inshaallah inshaallah iy
 jme3te kanmote khbar kidayer l3dou la3be lek lhal li lmoute lwer9a ma3autechlik 
 makaynch man mazal men mezian n3ass nass nour nta ounta parcours pieds plus rah 
 rak rani sa3a sa7bi sa7eb sahlen sbah sidi tan3ess tou7achnak twelef wata yhfde yhfdek zine'''))
+    print(moroccan_to_arabic(''' 3aransia 3ebass 3la 3lache 3lak 3jbek ach achno 
+ahlen alkhir allah amine amine ana ana ans atkounou awedi aychre7 b9aw ba bikher 
+casa chadha couiya course da7k daba dakchi de7k dial dwa et ewa fen fin fin 
+ghi had hada hadi hamdolillah hmd hmed hna ilyass incha inshaallah inshaallah iyeh 
+jme3te kanmote khbar kidayer l3dou la3be lek lhal li lmoute lwer9a ma3autechlik 
+makaynch man mazal men mezian n3ass nass nour nta ounta parcours pieds plus rah 
+rak rani sa3a sa7bi sa7eb sahlen sbah sidi tan3ess tou7achnak twelef wata yhfde yhfdek zine''' ))
+    print(moroccan_to_arabic('''Fen a 3emo'''))
+
 
 run_tests()
---
+++ 3aransia/constants.py
@@ -0,0 +1,3 @@
+CONTROLLED_DICTIONARY = 'controlled_dictionary.csv'
+OPEN_DICTIONARY = 'open_dictionary.csv'
+MOROCCAN_ALPHABET = 'moroccan_alphabet.csv'
