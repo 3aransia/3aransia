257d6e32ce83af2c46cc5a4cab5eabf1beae98f9
Amine M. Boulouma <amine@boulouma.com>
2020-01-29 23:31:45 +0100

    Update algorithms.py

--- _3aransia/algorithms.py
+++ _3aransia/algorithms.py
@@ -6,20 +6,6 @@ import nltk
 
 from _3aransia.constants import *
 
-
-# Function to compute the distance between two words
-def word_distance(word_1, word_2):
-    return nltk.edit_distance(word_1, word_2)
-
-# Generate lexically close words
-def generate_close_words(threshold, _str):
-    words = set()
-    for w in _str.split():
-        for y in _str.split():
-            if w != y and word_distance(w,y) < threshold: 
-                words.add((w,y, word_distance(w,y)))
-    return sorted(words, key=lambda x:len(x[0]))
-
 # Validate Latin/Digit Moroccan to Arabic dictionary
 def validate_dictionary(dictionary):
     data = pd.read_csv(dictionary)
