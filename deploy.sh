c764d7a7537c35560dd55cbdd5eb7ec69371efe8
Amine M. Boulouma <amine@boulouma.com>
2020-03-06 08:11:06 +0100

    Deployment

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,20 @@
 #!/bin/zsh
 # chmod +x deploy.sh
 # Update software version in Setup.py first
-rm -rf dist/* && python setup.py sdist && twine upload dist/*    
-git add . && git commit -m $1 && git push 3aransia develop && git push 3aransia master
+rm -rf dist/* 
+python setup.py sdist 
+twine upload dist/*    
+git add . 
+git commit -m $1 
+git push 3aransia develop 
+git push 3aransia master
 sleep 1m
 cd ../3aransia.api
 source venv/bin/activate   
 pip install --upgrade aaransia    
 pip freeze > requirements.txt
-git add . && git commit -m $1 && git push 3aransia develop && git push 3aransia master
+git add . 
+git commit -m $1 
+git push 3aransia develop 
+git push 3aransia master
 cd ../3aransia
